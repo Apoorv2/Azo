907aaa07bc81c8a6d7a9c6a31b79e68fcb0ef1f5
anmol.gupta4 <anmol.gupta4@flipkart.com>
2023-08-26 12:57:25 +0530

    Updated Indexing

--- src/components/Navbars/AuthNavbar.js
+++ src/components/Navbars/AuthNavbar.js
@@ -85,13 +85,12 @@ export default function Navbar(props) {
               <li className="flex items-center">
                 <button
                   className="bg-white text-blueGray-700 active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
-                  type="button"
-                >
-                  <i className="fas fa-arrow-alt-circle-down"> <Link
-          to="/auth/login"
-        >
-        Contact Us
-        </Link> </i> 
+                  type="button">
+                  <i className="fas fa-arrow-alt-circle-down">
+                    <Link to="/auth/login">
+                      Get Started
+                    </Link>
+                  </i>
                 </button>
               </li>
             </ul>
